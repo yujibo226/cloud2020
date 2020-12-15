c7f6b4f48529bdad09594a4b9d97f3877a222026
yujibo <4423820@qq.com>
2020-12-15 09:57:20 +0800

    OpenFeign代码开发维护

---
+++ cloud-consumer-feign-order80/src/main/java/com/yujibo/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,27 @@
+package com.yujibo.springcloud.controller;
+
+
+import com.yujibo.springcloud.entities.CommonResult;
+import com.yujibo.springcloud.entities.Payment;
+import com.yujibo.springcloud.service.PaymentFeignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderFeignController {
+
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+        return paymentFeignService.getPaymentById(id);
+    }
+
+}
---
+++ cloud-consumer-feign-order80/src/main/java/com/yujibo/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,17 @@
+package com.yujibo.springcloud.service;
+
+import com.yujibo.springcloud.entities.CommonResult;
+import com.yujibo.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+
+}
